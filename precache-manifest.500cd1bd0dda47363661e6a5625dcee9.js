2c46a94d88c893202affc8d689a8077fbb247ea5
Hayley Tong <hayleytong@gmail.com>
2020-06-25 11:31:04 +0100

    Updates

---
+++ precache-manifest.500cd1bd0dda47363661e6a5625dcee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68403e29aef7586f90c5d02144b886be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f40c432bc8b341a16ba",
+    "url": "/static/css/main.789a9242.chunk.css"
+  },
+  {
+    "revision": "68d0ae7711be9288a79d",
+    "url": "/static/js/2.0ace7ab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ace7ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f40c432bc8b341a16ba",
+    "url": "/static/js/main.9f89570f.chunk.js"
+  },
+  {
+    "revision": "b57a16e94aa78e2a99f6",
+    "url": "/static/js/runtime-main.54722b34.js"
+  }
+]);
