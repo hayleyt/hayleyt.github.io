74f03048a1545fafe334e4e82299b3d67d0abaa0
Hayley Tong <hayleytong@gmail.com>
2020-06-30 18:45:56 +0100

    added CSSGrid to portfolio section for better mobile support. Added Todolist project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,26 +4,22 @@ import Project from './Project'
 function Portfolio() {
     return(
         <div className="portfolio" id="PORTFOLIO">
-            <div className="container">
-                <div className="row">
-                    <div className="column portfolio-title">
-                        <h3>PORTFOLIO</h3>
-                        <h2>Recent Projects</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="column portfolio-column">
-                        <Project 
-                            img="tictactoe.jpg"
-                            title="Tic Tac Toe game"
-                            desc="Based on the reactjs tutorial with added features"
-                            link="https://hayleyt.github.io/tic-tac-toe"
-                        />
-                    </div>
-                    <div className="column portfolio-column">
-                    </div>
-                </div>
+            <div className="portfolio-title">
+                <h3>PORTFOLIO</h3>
+                <h2>Recent Projects</h2>
             </div>
+            <Project 
+                img="todo-list.jpg"
+                title="Todo List App"
+                desc="Designed for mobile. Toggle for Dark and Light mode. Built with React."
+                link="https://hayleyt.github.io/todo-list"
+            />
+            <Project 
+                img="tictactoe.jpg"
+                title="Tic Tac Toe game"
+                desc="Based on the reactjs tutorial with added features"
+                link="https://hayleyt.github.io/tic-tac-toe"
+            />
         </div>
     )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Project(props) {
     return(
-        <div className="portfolio-item">
-            <a href={props.link}>
-                <span className="caption">
-                    <h2>{props.title}</h2>
-                    <h3>{props.desc}</h3>
-                </span>
+        <div className="project">
+            <a href={props.link} className="project-img">
                 <img src={props.img} alt={props.title}/>
             </a>
+            <div className="project-desc">
+                <h3>{props.title}</h3>
+                <div>{props.desc}</div>
+            </div>
         </div>
     )
 }
