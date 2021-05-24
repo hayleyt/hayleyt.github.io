f2092ae05a624292c8728c40a6273d4de285182f
Hayley Tong <hayley@Hayleys-MacBook-Air.local>
2021-05-24 15:40:14 +0100

    fixes copyright and comments out redundant icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ function Footer () {
    return (
       <footer className="footer row">
          <div className="container">
-            <div className="copyright">Copyright © Hayley Tong 2020</div>
+            <div className="copyright">Copyright © Hayley Tong 2021</div>
             <div>hayleytong@gmail.com</div>
             <div className="social">
-               <a href="https://hayleyt.github.com"><i className="fab fa-github"></i></a>
-               <a href="https://hayleyt.github.com"><i className="fab fa-medium-m"></i></a>
-               <a href="https://hayleyt.github.com"><i className="fab fa-linkedin-in"></i></a>
+               <a href="https://github.com/hayleyt"><i className="fab fa-github"></i></a>
+               {/* <a href="https://github.com/hayleyt"><i className="fab fa-medium-m"></i></a>
+               <a href="https://github.com/hayleyt"><i className="fab fa-linkedin-in"></i></a> */}
             </div>
          </div>
       </footer>
