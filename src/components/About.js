626df039c528e4ebf847ec673d85aaa45df5a4b8
Hayley Tong <hayley@Hayleys-Air.localdomain>
2021-06-03 17:07:11 +0100

    updates about me section and fixes github link in footer

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,30 @@ function About () {
          <div className="post">
             <h2>About me</h2>
             <p>
-               I'm a front end engineer based in London, UK. I'm also studying for a bachelor's in Data Science at The Open University.
+               I'm a computing and statistics student studying part-time with the Open University. 
+               I'm looking to move into a new career in software development. I'm mostly 
+               self taught with a strong interest in design and front-end technologies, 
+               particularly Javascript, React and Vue. I have experience in designing, 
+               building and hosting Wordpress websites and eccommerce sites for small 
+               businesses.
+            </p>
+            <p>
+               I'm keen to secure an entry level position in a technology company with 
+               opportunities for progression.
+            </p>
+            <h2>Skills</h2>
+            <p>
+               <ul>
+                  <li>HTML, CSS, Sass</li>
+                  <li>Responsive web design with media queries</li>
+                  <li>Design and coding for Accessibility</li>
+                  <li>Javascript, ReactJS, and VueJS</li>
+                  <li>Java and Object-oriented programming</li>
+                  <li>Working with APIs</li>
+                  <li>Basic level Python</li>
+                  <li>Git source control</li>
+                  <li>Adobe Photoshop, and Illustrator</li>
+               </ul>
             </p>
          </div>
       </div>
